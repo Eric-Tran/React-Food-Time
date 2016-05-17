e16a493961f759f46bb69cca393bb7803205cf4c
Eric <to.erictran@gmail.com>
2016-05-16 18:44:50 -0700

    added form html

--- client/src/components/yelp_list_item.js
+++ client/src/components/yelp_list_item.js
@@ -3,6 +3,7 @@ import { yelpData } from '../actions/index';
 import { reduxForm } from 'redux-form';
 import { bindActionCreators } from 'redux';
 import { Modal } from 'react-bootstrap';
+import * as actions from '../actions';
 
 class YelpList extends Component {
 	constructor(props) {
@@ -12,6 +13,12 @@ class YelpList extends Component {
 		// this.close = this.close.bind(this);
 		// this.open = this.open.bind(this);
 	}
+	handleFormSubmit({ id, day, arrival, wait }) {
+		this.props.postWait( { id, day, arrival, wait })
+		this.setState({showModal: false})
+
+	}
+
 	close() {
 		console.log('closed');
     this.setState({showModal: false});
@@ -22,6 +29,7 @@ class YelpList extends Component {
  	}
 
 	render() {
+		const { handleSubmit, fields: { id, day, wait, arrival }} = this.props;
 		return (
 		<li className="list-group-item list-spacing">
 			<div className="media">
@@ -41,59 +49,74 @@ class YelpList extends Component {
 						Add Wait Time
 					</button>
 					<Modal show={this.state.showModal} onHide={this.close.bind(this)}>
-          			<Modal.Header closeButton>
-            			<Modal.Title>Add Wait Time</Modal.Title>
-          			</Modal.Header>
-          			<Modal.Body>
-			         	<form className="form-inline">
-				         	<fieldset className="form-group">
-				         		<label>Arrived Approximately:</label>
-				         		<select className="form-control">
-				         			<option value="1 AM">1 AM</option>
-				         			<option value="2 AM">2 AM</option>
-				         			<option value="3 AM">3 AM</option>
-				         			<option value="4 AM">4 AM</option>
-				         			<option value="5 AM">5 AM</option>
-				         			<option value="6 AM">6 AM</option>
-				         			<option value="7 AM">7 AM</option>
-				         			<option value="8 AM">8 AM</option>
-				         			<option value="9 AM">9 AM</option>
-				         			<option value="10 AM">10 AM</option>
-				         			<option value="11 AM">11 AM</option>
-				         			<option value="12 PM">12 PM</option>
-				         			<option value="1 PM">1 PM</option>
-				         			<option value="2 PM">2 PM</option>
-				         			<option value="3 PM">3 PM</option>
-				         			<option value="4 PM">4 PM</option>
-				         			<option value="5 PM">5 PM</option>
-				         			<option value="6 PM">6 PM</option>
-				         			<option value="7 PM">7 PM</option>
-				         			<option value="8 PM">8 PM</option>
-				         			<option value="9 PM">9 PM</option>
-				         			<option value="10 PM">10 PM</option>
-				         			<option value="11 PM">11 PM</option>
-				         			<option value="12 AM">12 AM</option>
-				         		</select>
-				         	</fieldset>
-			         		<hr />
-			         		<fieldset className="form-group">
-				         		<label>Wait is less than:</label>
-				         		<select className="form-control">
-				         			<option value="10 minutes">10 minutes</option>
-				         			<option value="20 minutes">20 minutes</option>
-				         			<option value="30 minutes">30 minutes</option>
-				         			<option value="45 minutes">45 minutes</option>
-				         			<option value="1 hour">1 hour</option>
-				         			<option value="1 hour 30 minutes">1 hour 30 minutes</option>
-				         			<option value="2 hours">2 hours</option>
-				         			<option value="2 hours+">2 hours+</option>
-				         		</select> 
-			         		</fieldset>
-			         	</form>
-          			</Modal.Body>
-          			<Modal.Footer>
-            			<button onClick={this.close.bind(this)}>Close</button>
-         			</Modal.Footer>
+          			<form className="form-inline" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+	          			<Modal.Header closeButton>
+	            			<Modal.Title>Add Wait Time</Modal.Title>
+	          			</Modal.Header>
+	          			<Modal.Body>
+	          					<input {...id} type="hidden" name="id" value={this.props.data.id} />
+					         	<fieldset className="form-group">
+					         		<label>Arrived Approximately:</label>
+					         		<select className="form-control" {...arrival} >
+					         			<option value="1">1 AM</option>
+					         			<option value="2">2 AM</option>
+					         			<option value="3">3 AM</option>
+					         			<option value="4">4 AM</option>
+					         			<option value="5">5 AM</option>
+					         			<option value="6">6 AM</option>
+					         			<option value="7">7 AM</option>
+					         			<option value="8">8 AM</option>
+					         			<option value="9">9 AM</option>
+					         			<option value="10">10 AM</option>
+					         			<option value="11">11 AM</option>
+					         			<option value="12">12 PM</option>
+					         			<option value="13">1 PM</option>
+					         			<option value="14">2 PM</option>
+					         			<option value="15">3 PM</option>
+					         			<option value="16">4 PM</option>
+					         			<option value="17">5 PM</option>
+					         			<option value="18">6 PM</option>
+					         			<option value="19">7 PM</option>
+					         			<option value="20">8 PM</option>
+					         			<option value="21">9 PM</option>
+					         			<option value="22">10 PM</option>
+					         			<option value="13">11 PM</option>
+					         			<option value="24">12 AM</option>
+					         		</select>
+					         	</fieldset>
+					         	<fieldset className="form-group">
+					         		<label className="spacing">On:</label>
+					         		<select className="form-control" {...day} >
+					         			<option value="Monday">Monday</option>
+					         			<option value="Tuesday">Tuesday</option>
+					         			<option value="Wednesday">Wednesday</option>
+					         			<option value="Thursday">Thursday</option>
+					         			<option value="Friday">Friday</option>
+					         			<option value="Saturday">Saturday</option>
+					         			<option value="Sunday">Sunday</option>
+					         		</select>
+					         	</fieldset>
+				         		<hr />
+				         		<fieldset className="form-group">
+					         		<label>Wait was less than:</label>
+					         		<select className="form-control" {...wait} >
+					         			<option value="10 minutes">10 minutes</option>
+					         			<option value="20 minutes">20 minutes</option>
+					         			<option value="30 minutes">30 minutes</option>
+					         			<option value="45 minutes">45 minutes</option>
+					         			<option value="1 hour">1 hour</option>
+					         			<option value="1 hour 15 minutes">1 hour 15 minutes</option>
+					         			<option value="1 hour 30 minutes">1 hour 30 minutes</option>
+					         			<option value="2 hours">2 hours</option>
+					         			<option value="2 hours+">2 hours+</option>
+					         		</select> 
+				         		</fieldset>
+	          			</Modal.Body>
+	          			<Modal.Footer>
+	          				<button action="submit" className="btn btn-primary btn-spacing">Submit</button>
+	            			<button className="btn btn-default" onClick={this.close.bind(this)}>Close</button>
+	         			</Modal.Footer>
+         			</form>
         			</Modal>
 					<span> Wait Time:</span>
 				</div>
@@ -111,6 +134,6 @@ function mapDispatchToProps(dispatch) {
 
 
 export default reduxForm({
-	form: 'wait',
-	fields: ['wait_time']
-}, mapDispatchToProps)(YelpList);
+	form: 'waitForm',
+	fields: ['id', 'day', 'arrival', 'wait']
+}, mapDispatchToProps, actions)(YelpList);
