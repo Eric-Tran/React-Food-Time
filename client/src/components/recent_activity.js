0bf1f7d49ddf0888f84c17f1a00b33d3ec6125fa
Eric <to.erictran@gmail.com>
2016-05-25 12:47:56 -0700

    completed recent activity component

--- client/src/components/recent_activity.js
+++ client/src/components/recent_activity.js
@@ -6,15 +6,16 @@ export default class RecentActivity extends Component {
 		if(this.props.activity){
 			return this.props.activity.map((data) => {
 				if (data.term !== undefined){
+					let time = data.created_at
 					return (
 						<li key={data._id}>
-							{data.term} 
+							<span className="wait">{data.term}</span> searched for in <span className="wait">{data.location}</span> {this.time}
 						</li>
 					)
 				}
 					return (
 						<li key={data._id}>
-							{data.business_name}
+							<span className="wait">{data.wait_time}</span> wait on <span className="wait">{data.day} at {data.arrival_time}</span> has been added to <span className="wait">{data.business_name}</span> {this.time}
 						</li>
 					)
 			})
@@ -25,12 +26,10 @@ export default class RecentActivity extends Component {
 		console.log("this is the datassssss", this.props.activity);
 		return (
 			<div>
-			<Well>
 				<h4>Recent Activity</h4>
 				<ul>
 				{this.renderActivity()}
 				</ul>
-			</Well>
 			</div>
 		)
 	}
