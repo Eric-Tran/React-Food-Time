5c2ae1f1e977181ff1240737c441892ca19cf0da
Eric <to.erictran@gmail.com>
2016-06-01 21:14:54 -0700

    comment out defineplugin since it slows dev

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,18 +9,18 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
-   plugins:[
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress:{
-        warnings: true
-      }
-    })
-  ],
+  //  plugins:[
+  //   new webpack.DefinePlugin({
+  //     'process.env':{
+  //       'NODE_ENV': JSON.stringify('production')
+  //     }
+  //   }),
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     compress:{
+  //       warnings: true
+  //     }
+  //   })
+  // ],
   module: {
     loaders: [{
       exclude: /node_modules/,
