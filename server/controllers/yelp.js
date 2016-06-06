8593105ee6832f6e2f1043fe26e56a14998a8c39
Eric <to.erictran@gmail.com>
2016-06-06 15:43:59 -0700

    server test

--- server/controllers/yelp.js
+++ server/controllers/yelp.js
@@ -54,6 +54,7 @@ exports.searchYelp = function(req, res, next) {
 						data.businesses[i].wait_data = [];
 						data.businesses[i].key = num++;
 						for (var x = 0; x < result.length; x++) {
+							console.log("this is date UTC", date, "this the offset", timeOffset, "this is the restuarant", result[x].business_id, "this is the saved day", result[x].day, "this is current day", dayOfWeek[day], "this is saved hour", result[x].arrival_time, "this is current hour", hour);
 							if (data.businesses[i].id == result[x].business_id) {
 								if (result[x].day == dayOfWeek[day] && result[x].arrival_time == hour) {
 									var estWait = result[x].wait_time;
